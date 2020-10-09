9d25511ac2439a7c08cad3165f7c353e1f7fb08a
Albreis <albreisdev@gmail.com>
2020-10-08 23:53:32 -0300

    Atualizada a interface IProject

--- src/Interfaces/IProject.php
+++ src/Interfaces/IProject.php
@@ -1,7 +1,5 @@
 <?php namespace Albreis\Kurin\Interfaces;
 
-use Albreis\Kurin\Models\Project;
-
 interface IProject {
   /**
    * get project name
@@ -11,7 +9,7 @@ interface IProject {
   /**
    * set project name
    */
-  public function setName(): string;
+  public function setName(string $name);
 
   /**
    * get projct description
@@ -21,65 +19,25 @@ interface IProject {
   /**
    * set projct description
    */
-  public function setDescription(): string;
-
-  /**
-   * get all projects
-   * 
-   */
-  public function getAll(): array;
+  public function setDescription(string $description);
 
   /**
    * get all projects done
    * 
    */
-  public function getDone(): array;
+  public function setDone();
 
   /**
    * get all projects open
    * 
    */
-  public function getOpen(): array;
+  public function setOpen();
 
   /**
    * get all projects open
    * 
    */
-  public function getDeleted(): array;
-
-  /**
-   * get all projects by date
-   * 
-   */
-  public function getByDate(DateTime $date): array;
-
-  /**
-   * get all projects by user id
-   * 
-   */
-  public function getByUserId(int $user_id): array;
-
-  /**
-   * get all projects by user state
-   * 
-   * @param string $state done|open|deleted
-   * 
-   */
-  public function getByState(string $state): array;
-
-  /**
-   * set ordering by
-   * 
-   * @param string $field
-   * @param string $pos
-   */
-  public function setOrderBy(string $field, string $pos);
-
-  
-  /**
-   * get ordering by
-   */
-  public function getOrderBy(string $field, string $pos): array;
+  public function setDeleted();
 
   /**
    * set all tasks done
@@ -96,10 +54,4 @@ interface IProject {
    */
   public function deleteAllTasks();
 
-  /**
-   * get project by ID
-   * 
-   * @return Albreis\Kurin\Models\Project
-   */
-  public function getProjectById(int $id): Project;
 }
