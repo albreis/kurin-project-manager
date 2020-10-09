a2d63f9cbfef4df72a63b912b58f9be95628a08b
Albreis <albreisdev@gmail.com>
2020-10-08 23:53:12 -0300

    Criada trait deconexão com banco de dados

---
+++ src/Traits/DBConnection.php
@@ -0,0 +1,18 @@
+<?php namespace Albreis\Kurin\Traits;
+
+use PDO;
+
+trait DBConnection {
+  
+  private $dsn = 'mysql:host=162.241.2.93;dbname=albreis2_kurin';
+  private $username = 'albreis2_kurin';
+  private $password = 'C!V3wMrgN[NY';
+  private $options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+  ); 
+  public $db;
+
+  public function connect() {
+    $this->db = new PDO($this->dsn, $this->username, $this->password, $this->options);
+  }
+}
