a4e4f3dbb760cb27491dea9ab31a1cca65e4f22f
Albreis <albreisdev@gmail.com>
2020-10-08 23:53:55 -0300

    Criado model Project

--- src/Models/Project.php
+++ src/Models/Project.php
@@ -1,5 +1,43 @@
-<?php namespace Albreis\Kurin\Models;
+<?php
 
-class Project {
+namespace Albreis\Kurin\Models;
+
+class Project implements \Albreis\Kurin\Interfaces\IProject
+{
+  use \Albreis\Kurin\Traits\DBConnection;
+
+  public string $name;
+  public string $description;
+  public array $tasks;
+
+  public function getName(): string { 
+    return $this->name;
+  }
+
+  public function setName(string $name) {
+    $this->name = $name;
+  }
+
+  public function getDescription(): string {
+    return $this->description;
+  }
+
+  public function setDescription(string $description) { 
+    $this->description = $description;
+  }
+
+  public function setDone() {}
+
+  public function setOpen() { }
+
+  public function setDeleted() { }
+
+  public function doneAllTasks() { }
+
+  public function openAllTasks() { }
+
+  public function deleteAllTasks() { }
+
+  
   
 }
