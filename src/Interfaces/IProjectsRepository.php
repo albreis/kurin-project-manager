a3bce3dfa241c33f5a45b1ec0cfed670f17aa2a3
Albreis <albreisdev@gmail.com>
2020-10-08 23:54:20 -0300

    Criado interface do repositório de projetos

---
+++ src/Interfaces/IProjectsRepository.php
@@ -0,0 +1,59 @@
+<?php namespace Albreis\Kurin\Interfaces;
+
+use Albreis\Kurin\Models\Project;
+
+interface IProjectsRepository {
+
+  /**
+   * get all projects
+   * 
+   */
+  public function getAll(): array;
+
+  /**
+   * get all projects open
+   * 
+   */
+  public function getDeleted(): array;
+
+  /**
+   * get all projects by date
+   * 
+   */
+  public function getByDate(\DateTime $date): array;
+
+  /**
+   * get all projects by user id
+   * 
+   */
+  public function getByUserId(int $user_id): array;
+
+  /**
+   * get all projects by user state
+   * 
+   * @param string $state done|open|deleted
+   * 
+   */
+  public function getByState(string $state): array;
+
+  /**
+   * set ordering by
+   * 
+   * @param string $field
+   * @param string $pos
+   */
+  public function setOrderBy(string $field, string $pos);
+
+  
+  /**
+   * get ordering by
+   */
+  public function getOrderBy(): array;
+
+  /**
+   * get project by ID
+   * 
+   * @return Albreis\Kurin\Models\Project
+   */
+  public function getProjectById(int $id): Project;
+}
