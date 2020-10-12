c7bb91d9ab8fb0684fa5abb4f3964f1909944a30
Albreis <albreisdev@gmail.com>
2020-10-12 03:47:01 -0300

    Refactoring

--- src/Models/Project.php
+++ src/Models/Project.php
@@ -4,7 +4,6 @@ namespace Albreis\Kurin\Models;
 
 use Albreis\Kurin\Interfaces\Models\IProject;
 use Albreis\Kurin\Model;
-use Albreis\Kurin\Traits\InformationExtractor;
 
 /** @package Albreis\Kurin\Models */
 class Project extends Model implements IProject
@@ -12,44 +11,14 @@ class Project extends Model implements IProject
 
   public string $name;
   public ?string $description = null;
-  protected array $tasks = [];
 
-  /** @return string  */
-  public function getName(): string { 
-    return $this->name;
-  }
-
-  /**
-   * @param string $name 
-   * @return void 
-   */
-  public function setName(string $name) {
-    $this->name = $name;
-  }
-
-  /** @return string  */
-  public function getDescription(): string {
+  public function getDescription(): ?string { 
     return $this->description;
   }
 
-  /**
-   * @param string $description 
-   * @return void 
-   */
-  public function setDescription(string $description) { 
-    $this->description = $description;
+  public function getName(): ?string
+  {
+    return $this->name;
   }
-
-  public function setDone() {}
-
-  public function setOpen() { }
-
-  public function setDeleted() { }
-
-  public function doneAllTasks() { }
-
-  public function openAllTasks() { }
-
-  public function deleteAllTasks() { }
   
 }
