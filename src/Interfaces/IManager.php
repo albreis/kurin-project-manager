c9cb3ac283915d2d0446f6e1e684d6b61bed5fe3
Albreis <albreisdev@gmail.com>
2020-10-12 03:44:14 -0300

    Create Manager interface

---
+++ src/Interfaces/IManager.php
@@ -0,0 +1,9 @@
+<?php namespace Albreis\Kurin\Interfaces;
+
+/** @package Albreis\Kurin\Interfaces */
+interface IManager {
+  
+  /** @return null|string  */
+  public function getName(): ?string;
+
+}
