64057217c028f4c7d775edfdc3e1410ae12abac0
Albreis <albreisdev@gmail.com>
2020-10-12 04:07:00 -0300

    Refactoring

--- src/Validator.php
+++ src/Validator.php
@@ -5,20 +5,31 @@ use Albreis\Kurin\Interfaces\IValidator;
 /** @package Albreis\Kurin */
 abstract class Validator implements IValidator {
 
-  private object $object;
+  private ?object $object;
+  private ?array $validObjects;
+  private ?array $invalidObjects;
   private ?array $errors = [];
   
   /**
    * @param object $object 
    * @return void 
    */
-  public function __construct(object $object){
+  public function __construct(?object $object = null){
     $this->object = $object;
   }
 
   public function getErrors(): ?array
   {
     return $this->errors;
+  }
+
+  public function addObject(object $object) {
+    $this->objects[] = $object;
+    return $this;
+  }
+  public function addObjects(array $objects) {
+    $this->objects = array_merge($this->objects, $objects);
+    return $this;
   }
 
 }
