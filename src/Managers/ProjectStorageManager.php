720ac6530df508d4eb58fe6c39cf18e8b597ac40
Albreis <albreisdev@gmail.com>
2020-10-12 03:46:28 -0300

    Create ProjectStorageManager

---
+++ src/Managers/ProjectStorageManager.php
@@ -0,0 +1,24 @@
+<?php namespace Albreis\Kurin\Managers;
+
+/** @package Albreis\Kurin\Managers */
+class ProjectStorageManager extends StorageManager {
+
+  /**
+   * @param bool $return 
+   * @return null|object 
+   * @throws PDOException 
+   */
+  public function store(object $project, bool $return =  true): ?int { 
+    $sql = "INSERT INTO projects(name, created_at) VALUES (:name, :created_at)";
+    $data = [
+      'name' => $project->getName(),
+      'created_at' => $project->getCreatedAt()->format('Y-m-d H:i:s'),
+    ];
+    $this->storage->query($sql, $data);
+    if($return) {
+      return $this->storage->lastInsertId();
+    }
+    return null;
+  }
+
+}
